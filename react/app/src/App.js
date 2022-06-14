d574c962f22b9db5f437c0266f2376bec931a1be
Timerson Downing <tldown2222@gmail.com>
2022-06-14 08:08:15 -0500

    React App First Commit

---
+++ react/app/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <p>* Learn ES6</p>
+      <p>* Learn JSX</p>
+      <p>* Go for a Hike</p>
+    </div>
+  );
+}
+
+export default App;
