3ca386d0393320b2f5037615fd48b9f54ce04ab0
Timerson Downing <tldown2222@gmail.com>
2022-06-14 11:34:48 -0500

    Putting It Together

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,18 +1,13 @@
-
 import './App.css';
 import Person from './components/Person';
-
-
-
-
 
 function App() {
   return (
     <div className="App">
-      <Person firstName='Bill' lastName='Smith' age='25' hairColor='Blue'/>
-      <Person firstName='Bugs' lastName='Bunny' age='100' hairColor='Grey'/>
-      <Person firstName='Lone' lastName='Ranger' age='80' hairColor='Black'/>
-      <Person firstName='Ed' lastName='Smith' age='25' hairColor='Green'/>
+      <Person firstName='Bill' lastName='Smith' age={25} hairColor='Blue'/>
+      <Person firstName='Bugs' lastName='Bunny' age={100} hairColor='Grey'/>
+      <Person firstName='Lone' lastName='Ranger' age={80} hairColor='Black'/>
+      <Person firstName='Ed' lastName='Smith' age={25} hairColor='Green'/>
     </div>
   );
 }
--- react/app/src/components/Person.js
+++ react/app/src/components/Person.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Person = (props) => {
+    const [personAge, setPersonAge] = useState(props.age);
+
     return (
         <div>
             <h1>{props.firstName} {props.lastName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {personAge}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={ (event)=> setPersonAge(personAge + 1) }>Had A Birthday!</button>      
         </div>
     )
 }
