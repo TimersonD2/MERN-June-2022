e01fc10cba66f244e941c02b8f01386d6ad324d2
Timerson Downing <tldown2222@gmail.com>
2022-06-28 18:35:42 -0500

    Axios Commit

--- react/pokemon/src/App.js
+++ react/pokemon/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -7,21 +8,24 @@ function App() {
   // https://pokeapi.co/api/v2/pokemon/ditto
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-      .then((res) => res.json())
-      .then((result) => setPokemon(result.results))
+    // fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+    //   .then((res) => res.json())
+    //   .then((result) => setPokemon(result.results))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+      .then((response) => {setPokemon(response.data.results)})
       .catch((err) => console.log("Error is: ", err))
   }, []);
 
   return (
     <div className="App">
-      <h1>Pokemon API App</h1>
-      {pokemon.map((item, index) => (
-        <div key={index}>
-          <li>{item.name}</li>
-        </div>
-      ))}
-
+      <h1>Pokemon</h1>
+      {
+        pokemon.map((item, index) => (
+          <div key={index}>
+            <li>{item.name}</li>
+          </div>
+        ))
+      }
     </div>
   );
 }
